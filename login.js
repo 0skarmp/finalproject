57f2f4ce864d1020258031f74890b5ccf8f11d72
JuanRCoder <guillecarck@gmail.com>
2023-10-01 23:21:15 -0500

    feat: modified files

--- login.js
+++ login.js
@@ -1,6 +1,22 @@
-let iniciarSesion = document.querySelector('.btn_change');
+let btnRegister = document.querySelector('.btn_register');
+let formRegister = document.querySelector('.col_1-1');
+let boxLogin = document.querySelector('.col_2-1');
 
-iniciarSesion.addEventListener('click', ()=>{
-    document.querySelector('.form_login').style.display = "none";
 
+let btnLogin = document.querySelector('.btn_login');
+let formLogin = document.querySelector('.col_1');
+let boxRegister = document.querySelector('.col_2');
+
+btnRegister.addEventListener('click', ()=> {
+    formRegister.classList.add('col_1-1-change');
+    boxLogin.classList.add('col_2-1-change');
+    formLogin.style.display = "none";
+    boxRegister.style.display = "none";
+});
+
+btnLogin.addEventListener('click', ()=> {
+    formRegister.classList.remove('col_1-1-change');
+    boxLogin.classList.remove('col_2-1-change');
+    formLogin.style.display = "block";
+    boxRegister.style.display = "flex";
 });
