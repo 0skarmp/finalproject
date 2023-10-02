3dc98659741d2ebd9e7135ec194e1686038cbbe4
JuanRCoder <guillecarck@gmail.com>
2023-10-02 17:13:42 -0500

    feat: page login/register completed and responsive

--- login.js
+++ login.js
@@ -8,15 +8,15 @@ let formLogin = document.querySelector('.col_1');
 let boxRegister = document.querySelector('.col_2');
 
 btnRegister.addEventListener('click', ()=> {
-    formRegister.classList.add('col_1-1-change');
-    boxLogin.classList.add('col_2-1-change');
-    formLogin.style.display = "none";
-    boxRegister.style.display = "none";
+    formLogin.classList.add('col_1_change');
+    boxRegister.classList.add('col_1_change');
+    formRegister.classList.add('col_2_change');
+    boxLogin.classList.add('col_2_change');
 });
 
 btnLogin.addEventListener('click', ()=> {
-    formRegister.classList.remove('col_1-1-change');
-    boxLogin.classList.remove('col_2-1-change');
-    formLogin.style.display = "block";
-    boxRegister.style.display = "flex";
+    formLogin.classList.remove('col_1_change');
+    boxRegister.classList.remove('col_1_change');
+    formRegister.classList.remove('col_2_change');
+    boxLogin.classList.remove('col_2_change'); 
 });
